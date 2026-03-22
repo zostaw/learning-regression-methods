0cf25118f7d055d7321816fe21614142acf39cb2
zostaw <zostaw@users.noreply.github.com>
2026-03-23 00:17:59 +0100

    Visualizing meaningful configurations for 1D k-means, simpliest case: 3 points 2 centroids. It would be really cool to find formula for arbitrary configuration of: nodes, centroids at least for 1D. I think for higher dimensions that is not trivial, you'd need to find sections. With 1D finding sections is just moving along the line.

--- k-means.rkt
+++ k-means.rkt
@@ -1,28 +1,16 @@
-#lang racket
+#lang rosette
 
 (require plot)
+(require (only-in racket/list [argmin racket-argmin]))
 
-
-(define (k-means datapoints number-centroids
-                 #:initial-centroids-positions [initial-centroids-positions #f]
+(define (k-means datapoints initial-centroids-positions
                  #:min-change-required [min-change-required 0.001]
                  #:max-iterations [max-iterations 50])
   (begin
     (define num-dimensions (vector-length (car datapoints)))
 
     
-    (define centroids
-      (if (not initial-centroids-positions)
-          (for/list ([i (range number-centroids)])
-            (build-vector num-dimensions (λ (_) (random))))
-          (if (not (equal? (length initial-centroids-positions)
-                          number-centroids))
-              (error
-               (format "length of initial-centroids-positions (~a) must be equal to number-centroids (~a), but wasn't"
-                       (length initial-centroids-positions)
-                       number-centroids))
-              initial-centroids-positions)))
-
+    (define centroids initial-centroids-positions)
     
     (define (datapoints-with-centroids centroids)
       (foldl
@@ -30,7 +18,7 @@
          (cons 
                (cons datapoint
                      (car
-                      (argmin cdr
+                      (racket-argmin cdr
                               (for/list ([(centroid i) (in-indexed centroids)])
                                 (cons i
                                       (foldl
@@ -85,15 +73,15 @@
     (values centroids (datapoints-with-centroids centroids))))
 
 
-(define (generate datapoints)
+(define (generate datapoints initial-centroids-positions)
 
   (define-values (centroids datapoints-with-centroids)
-    (k-means datapoints 2))
+    (k-means datapoints initial-centroids-positions))
 
 
   (define (pick-centroid . dim-components) ;; x, y, z...
     (cdr
-     (argmin car (map (λ (centroid)
+     (racket-argmin car (map (λ (centroid)
                         (cons
                          (for/fold ([acc 0])
                                    ([dim (min (length dim-components)
@@ -211,7 +199,9 @@
 
 
 
-
+#;
+  PLOTTING
+;#
 (define (plot-datapoints sillhuette-scores centroids k-means-vector
                          #:with-vec-field [with-vec-field #f]
                          #:with-centroid-points [with-centroid-points #t]
@@ -255,7 +245,7 @@
                             #:size 3)
                     '()))
           #:height 300
-          #:width 1500
+          #:width 850
           #:x-min -0.1
           #:x-max 1.1
           #:y-min -0.5
@@ -303,21 +293,252 @@
 
 
 
-(define datapoints
-  (list (vector 0.2 0.3)
-        (vector 0.5 0.5)
-        (vector 0.7 0.1)))
-
-
-#;(define datapoints
-    (list (vector 0.2)
-          (vector 0.5)
-          (vector 0.7)))
-
-(define-values (sillhuette-scores centroids k-means-vector) (generate datapoints))
-;(println sillhuette-scores)
-(plot-datapoints sillhuette-scores centroids k-means-vector)
 
 
 
+(define initial-centroids-positions
+  (list (vector 0.3)
+        (vector 0.5)))
+
+
+
+#;(if #t
+    (begin
+      (define datapoints
+        (list (vector 0.2)
+              (vector 0.5)
+              (vector 0.7)))
+
+      (define-values (sillhuette-scores centroids k-means-vector)
+             (generate datapoints initial-centroids-positions))
+      (println (plot-datapoints sillhuette-scores centroids k-means-vector))
+
+      (println sillhuette-scores)
+      (println (car (cddr (car sillhuette-scores)))))
+
+    (begin
+      (define-symbolic point-a point-b point-c real?)
+      (define datapoints
+        (list (vector point-a)
+              (vector point-b)
+              (vector point-c)))
+
+
+      (define sol
+        (solve
+         (begin
+           (assume (<= 0.0
+                       point-a))
+           (assume (<= point-a
+                       point-b))
+           (assume (<= point-b
+                       point-c))
+           (assume (<= point-c
+                       1.0))
+
+           (define-values (sillhuette-scores centroids k-means-vector)
+             (generate datapoints initial-centroids-positions))
+
+       
+           (assert (<= (car (cddr (car sillhuette-scores))) 1.0)))))
+      (println sol)))
+
+
+   (define datapoints
+        (list (vector 0.2 0.0)
+              (vector 0.5 0.0)
+              (vector 0.7 0.0)))
+
+
+#|
+  Terminator points:
+    s_1, s_n
+|#
+
+;(define ϵ 0.01)
+(define ϵ (/ 1 1000))
+
+(define (gen-plot vectors-with-ϵ)
+  (let ([x-min -0.1]
+        [x-max 1.1])
+    (display (plot
+              (list
+
+               (lines
+                (reverse
+                 (for/fold ([lst (list (vector 0 0))]) ([i  (in-range x-min (add1 x-max))])
+                   (match-define (vector x y) (first lst))
+                   (cons (vector i 0) lst))))
+
+               (point-label (vector 0 0) "" #:point-sym "|")
+               (point-label (vector 1 0) "" #:point-sym "|")
+
+               ; terminator points
+               (for/list ([vector-with-ϵ (list (car vectors-with-ϵ) (last vectors-with-ϵ))])
+                 (point-label (vector-with-ϵ-datapoint vector-with-ϵ) ""
+                              #:point-color 'darkgreen
+                              #:point-fill-color 'darkgreen
+                              #:point-sym 'fullcircle5
+                              #:color 'darkgreen
+                              #:point-size 10
+                              #:size 0.0))
+
+               ; terminator labels
+               (for/list ([vector-with-ϵ (list (car vectors-with-ϵ) (last vectors-with-ϵ))])
+                 (point-label (vector (- (vector-ref (vector-with-ϵ-datapoint vector-with-ϵ) 0) 0.015) -0.2)
+                              (format "~a" (let ([vec (vector-ref (vector-with-ϵ-datapoint vector-with-ϵ) 0)]
+                                    [vec-ϵ (vector-with-ϵ-ϵ vector-with-ϵ)])
+                                (if (not (vector-with-ϵ-ϵ vector-with-ϵ))
+                                    vec
+                                    (+ vec vec-ϵ))))
+                              #:point-color 'darkgreen
+                              #:point-fill-color 'darkgreen
+                              #:point-sym 'fullcircle5
+                              #:color 'darkgreen
+                              #:point-size 0.0
+                              #:size 20))
+
+               ; internal point arrows
+               (for/list ([vector-with-ϵ (cdr (reverse (cdr vectors-with-ϵ)))])
+                 (arrows (list (vector (+ (vector-ref
+                                           (vector-with-ϵ-datapoint vector-with-ϵ) 0)
+                                          (cond [(false? (vector-with-ϵ-ϵ vector-with-ϵ)) 0.0]
+                                                [(> (vector-with-ϵ-ϵ vector-with-ϵ) 0.0) 0.05]
+                                                [(< (vector-with-ϵ-ϵ vector-with-ϵ) 0.0) -0.05]))
+                                       0.3
+                                       )
+                               (let ([vec (vector-with-ϵ-datapoint vector-with-ϵ)])
+                                 (vector
+                                  (if (not (vector-with-ϵ-ϵ vector-with-ϵ))
+                                      (vector-ref vec 0)
+                                      (+ (vector-ref vec 0) (vector-with-ϵ-ϵ vector-with-ϵ)))
+                                  (+ (vector-ref vec 1) 0.05))))
+                         #:arrow-head-size-or-scale '(= 10)
+                         #:color 'darkblue))
+
+               ; internal points
+               (for/list ([vector-with-ϵ (cdr (reverse (cdr vectors-with-ϵ)))])
+                 (point-label (vector-with-ϵ-datapoint vector-with-ϵ)
+                              (real->decimal-string (vector-ref (vector-with-ϵ-datapoint vector-with-ϵ) 0)
+                                                    (exact-round (log ϵ 0.1)))
+                              #:point-color 'darkgreen
+                              #:point-fill-color 'darkgreen
+                              #:point-sym 'fullcircle5
+                              #:color 'darkgreen
+                              #:point-size 10
+                              #:size 0.0))
+             
+
+
+               ; internal point labels
+               (for/list ([vector-with-ϵ (cdr (reverse (cdr vectors-with-ϵ)))])
+                 (point-label (vector (+ (vector-ref (vector-with-ϵ-datapoint vector-with-ϵ) 0)
+                                         (cond [(false? (vector-with-ϵ-ϵ vector-with-ϵ)) 0.0]
+                                               [(> (vector-with-ϵ-ϵ vector-with-ϵ) 0.0) 0.05]
+                                               [(< (vector-with-ϵ-ϵ vector-with-ϵ) 0.0) -0.05]))
+                                      0.2)
+                              (let ([vec (vector-ref (vector-with-ϵ-datapoint vector-with-ϵ) 0)]
+                                    [vec-ϵ (vector-with-ϵ-ϵ vector-with-ϵ)])
+                                (cond
+                                  [(equal? #f vec-ϵ) (format "~a" vec)]
+                                  [(> vec-ϵ 0) (format "~a+ϵ" vec)]
+                                  [(< vec-ϵ 0) (format "~a-ϵ" vec)]
+                                  [else (format "~a" vec)]))
+                              #:point-color 'darkgreen
+                              #:point-fill-color 'darkgreen
+                              #:point-sym 'fullcircle5
+                              #:color 'darkgreen
+                              #:point-size 0.0
+                              #:size 20))
+               )
+              #:height 300
+              #:width 850
+              #:x-min x-min
+              #:x-max x-max
+              #:y-min -1
+              #:y-max 1
+              ))))
+
+
+(struct vector-with-ϵ (datapoint ϵ)
+  #:transparent)
+
+(define (make-vector-with-ϵ datapoint [ϵ #f])
+  (vector-with-ϵ datapoint ϵ))
+
+(define datapoints-combinations
+  (let ([terminator-left 0]
+        [terminator-right 1])
+    (for/fold ([acc '()])
+              ([rule (list 'at-terminator-left
+                           'at-terminator-right
+                           'one-third
+                           'two-thirds
+                           'first-range
+                           'mid-range
+                           'last-range)])
+      (match rule
+        
+        ['at-terminator-left
+         (cons 
+               (list (make-vector-with-ϵ (vector terminator-left 0))
+                     (make-vector-with-ϵ (vector terminator-left 0))
+                     (make-vector-with-ϵ (vector terminator-right 0)))
+               acc)]
+        
+        ['at-terminator-right
+         (cons 
+               (list (make-vector-with-ϵ (vector terminator-left 0))
+                     (make-vector-with-ϵ (vector terminator-right 0))
+                     (make-vector-with-ϵ (vector terminator-right 0)))
+               acc)]
+        
+        ['one-third
+         (cons 
+               (list (make-vector-with-ϵ (vector terminator-left 0))
+                     (make-vector-with-ϵ (vector (/ (+ terminator-right terminator-left) 3) 0))
+                     (make-vector-with-ϵ (vector terminator-right 0)))
+               acc)]
+
+        ['two-thirds
+         (cons 
+               (list (make-vector-with-ϵ (vector terminator-left 0))
+                     (make-vector-with-ϵ (vector (* 2 (/ (+ terminator-right terminator-left) 3)) 0))
+                     (make-vector-with-ϵ (vector terminator-right 0)))
+               acc)]
+        
+        ['first-range
+         (cons (list (make-vector-with-ϵ (vector terminator-left 0))
+                     (make-vector-with-ϵ (vector terminator-left 0) ϵ)
+                     (make-vector-with-ϵ (vector terminator-right 0)))
+               (cons (list (make-vector-with-ϵ (vector terminator-left 0))
+                           (make-vector-with-ϵ (vector (/ (+ terminator-left terminator-right) 3) 0) (- ϵ))
+                           (make-vector-with-ϵ (vector terminator-right 0)))
+                     acc))]
+        
+        ['mid-range
+         (cons (list (make-vector-with-ϵ (vector terminator-left 0))
+                     (make-vector-with-ϵ (vector (/ (+ terminator-left terminator-right) 3) 0) ϵ)
+                     (make-vector-with-ϵ (vector terminator-right 0)))
+               (cons (list (make-vector-with-ϵ (vector terminator-left 0))
+                           (make-vector-with-ϵ (vector (* 2 (/ (+ terminator-left terminator-right) 3)) 0) (- ϵ))
+                           (make-vector-with-ϵ (vector terminator-right 0)))
+                     acc))]
+        
+        ['last-range
+         (cons (list (make-vector-with-ϵ (vector terminator-left 0))
+                     (make-vector-with-ϵ (vector (* 2 (/ (+ terminator-left terminator-right) 3)) 0) ϵ)
+                     (make-vector-with-ϵ (vector terminator-right 0)))
+               (cons (list (make-vector-with-ϵ (vector terminator-left 0))
+                           (make-vector-with-ϵ (vector terminator-right 0) (- ϵ))
+                           (make-vector-with-ϵ (vector terminator-right 0)))
+                     acc))]
+        
+        ))))
+
+
+datapoints-combinations
+
+(for/list ([datapoints datapoints-combinations])
+  (gen-plot datapoints))
 
